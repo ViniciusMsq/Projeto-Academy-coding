7385e3a63f349760050e9109ad4ad472cfcfe27e
Vinicius Mesquini <viniciusm.gc@outlook.com>
2020-10-30 00:40:18 -0300

    atualizando scripts e incluindo css
    upgrade no projeto, mais um avanço

--- scripts.js
+++ scripts.js
@@ -1,22 +1,38 @@
 let btn = document.querySelector("#btn");
 let urlTeste = 'https://reqres.in/api/users?page=2'
 let urlTeste2 = 'https://api.hgbrasil.com/weather?format=json-cors&key=076a071b&city_name=';
-let obj
+let obj;
 
+let img = document.createElement("img");
+let cidade = document.getElementById("cidade");
+let data = document.getElementById("data");
+let temp = document.getElementById("temp");
+let humid = document.getElementById("humidade");
+let desc = document.getElementById("descricao");
 
 btn.addEventListener("click", function(){
     cityName = document.querySelector("#cityName");
     console.log(cityName.value)
+    
     fetch(urlTeste2+cityName.value)
     .then(function(response){
         return response.json();
-
     })
     .then(function(response){
-        obj  = response;
-        for(i = 0; i<10; i++){
-            console.log("Data: "+ obj['results']['forecast'][i]['date'] + " Max: " + obj['results']['forecast'][i]['max'] + " Min: " + obj['results']['forecast'][i]['min']);
-            
-        }
+        obj = response;
+
+        cidade.innerText+="  "+ obj['results']['city'];
+        data.innerText+="  "+ obj['results']['forecast'][0]['date'];
+        temp.innerText+="  Max: " + obj['results']['forecast'][0]['max']+"°" + "- Min: " + obj['results']['forecast'][0]['min']+"°";
+        humid.innerText+="  "+ obj['results']['humidity']+"%";
+        desc.innerText+="  "+ obj['results']['description'];
+        
+        img.src="https://assets.hgbrasil.com/weather/images/28n.png";
+
+        document.getElementById("imagem").appendChild(img);
+
+        
+        console.log(obj);
+        
     })
 })
